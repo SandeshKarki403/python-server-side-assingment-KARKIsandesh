4ade751eaf87b7f8b943b8dbdbf0b785d8e63e44
SandeshKarki403 <karkisandech@gmail.com>
2026-06-13 20:49:58 +0400

    Average.py

---
+++ average.py
@@ -0,0 +1,31 @@
+import csv
+
+with open("main.csv", "w") as file:
+    writer = csv.writer(file)
+    writer.writerow(["name", "marks"])
+    for i in range(4):
+        name = input("Enter student name: ")
+        marks = int(input("Enter marks of the student: "))
+        writer.writerow([name, marks])
+
+with open("main.csv", "r") as file:
+    reader = csv.reader(file)
+    main = list(reader)
+    
+    for row in main:
+        print(row)
+
+    print("If you want to have an average of the marks of students press 1")
+    press = int(input("Enter 1"))
+    
+    if press == 1:
+        total = 0
+        count = 0
+        for row in main[1:]:
+            if len(row) > 1: 
+                total += int(row[1])
+                count += 1
+        average = total / count
+        print("Average marks of students:", average)
+
+                
